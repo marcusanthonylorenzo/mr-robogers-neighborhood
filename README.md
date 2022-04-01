5232ab92747b210d79fc2a6455e7cf9b29165aaf
Marcus <marcanthonyzpadilla@gmail.com>
2022-04-01 09:26:08 -0700

    pass UI test: submit sends user input to BL function.

---
+++ README.md
@@ -0,0 +1,7 @@
+TESTS:
+
+Describe: robotTalk()
+
+Test: "It should return an array of numbers from 0 to the user's inputted number"
+Code: robotTalk(5);
+Expected Output: [0, 1, 2, 3, 4, 5]
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 //Business Logic
-
-
+function robotTalk(input){
+  console.log(input);
+  // return input *3;
+}
 
 
 //UI Logic
 $(document).ready(function () {
+  $("#form").submit(function (event) {
+    event.preventDefault();
 
+    //selectors
+    const numberInput = parseInt($("#numberInput").val());
+    const display = $(".display");
+    const submitBtn = $("#submit");
 
+    //submit action
+    let submit = robotTalk(numberInput);
+    display.append("<p>" + submit + "</p>");
+  });
 });
