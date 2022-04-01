5d825d779275637f8210d835568b2ddeff755643
Marcus <marcanthonyzpadilla@gmail.com>
2022-04-01 11:54:45 -0700

    WIP: write function to determine 1,2,3 vs 23 etc.

--- README.md
+++ README.md
@@ -2,7 +2,10 @@ TESTS:
 
 Describe: robotTalk(input)
 
-// *Store inputs AND output ["Beep", "Boop", "Won't you be my neighbor?"];
+<!-- *Store inputs AND output 
+[ "Beep" instead of 1,
+  "Boop" instead of 2,
+  "Won't you be my neighbor?" instead of 3]; -->
 
 <!-- Test: "It should take user input, run for loop and count each individual number up to specified number"
 Code: robotTalk(5);
@@ -12,9 +15,18 @@ Expected Output: (console:) 0, 1, 2, 3, 4, 5 -->
 Code: robotTalk(5);
 Expected Output: [0, 1, 2, 3, 4, 5] -->
 
-Test: "It should replace 1-11 "
+
+<!-- Test: "It should convert array to string"
+Code: arrayFromIndexInput.join(", ");
+Expected Output: "0, 1, 2, 3, 4, 5" -->
+
+Test: "It should check IF number is 1, 2, or 3, then replace with wordSubs[]"
 Code: robotTalk(5);
-Expected Output: [0, 1, 2, 3, 4, 5]
+Expected Output: [0, "Beep", "Boop", "Won't you be my neighbor?", 4, 5]
+
+Test: "It should replace 1-11 with wordSubs[i] "
+Code: robotTalk(5);
+Expected Output: [0, "Beep", "Boop", "Won't you be my neighbor?", 4, 5]
 
 
 Test:  If a user inputs a 13, the program should display a list of values starting with 0 to the user's inputted number, with the numbers 1, 2, 3, 10, 11, 12, 13 and 14 substituted with the correct phrase.
--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 //Business Logic
+
 function robotTalk(input){
   console.log(input);
-
   //starting statements
   const wordSubs = ["Beep", "Boop", "Won't you be my neighbor?"];
+  //store main array in const
+  const arrayIndexedInput = indexInput(input);
+  //convert array to str **convert in JQuery, or after robotTalk??
+  const convertToString = arrayIndexedInput.join(", ");
 
-  //expressions 
-  const arrayFromIndexInput = indexInput(input);
-  const arrayInputString = arrayFromIndexInput.join(", ");
-  
+  //replace 1,2,3 in array with wordSubs[i];
+
   //run iterator to count and index each number in array
   function indexInput(input){
     let arrayFromInput = [];
@@ -18,8 +20,29 @@ function robotTalk(input){
     return arrayFromInput;
   }
 
-  console.log(arrayFromIndexInput);
-  console.log(arrayInputString);
+  //check convertToString if 1, 2, 3 then replace with str in
+  function replaceStr123(arrayIndexedInput) {
+    let result;
+    for(let i = 0; i < arrayIndexedInput.length; i++){
+      if (arrayIndexedInput.includes(1)) {
+        arrayIndexedInput.splice(arrayIndexedInput.indexOf(1), 1, wordSubs[0]);
+        console.log(arrayIndexedInput);
+      } else if (arrayIndexedInput.includes(2)){
+        arrayIndexedInput.splice(arrayIndexedInput.indexOf(2), 1, wordSubs[1]);
+        console.log(arrayIndexedInput);
+      } else if (arrayIndexedInput.includes(3)){
+        arrayIndexedInput.splice(arrayIndexedInput.indexOf(3), 1, wordSubs[2]);
+        console.log(arrayIndexedInput);
+      } else {
+        return result;
+      }
+    };
+    return result;
+  }
+  replaceStr123(arrayIndexedInput);
+
+  console.log(arrayIndexedInput);
+  console.log(convertToString);
 }
 
 
