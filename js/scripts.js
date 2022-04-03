e81ef58e91b8c39ecbbf7a12fecd741fb492508d
Marcus <marcanthonyzpadilla@gmail.com>
2022-04-02 21:51:20 -0700

    achieve output requirements.

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,9 @@ function robotTalk(input){
 
   function replaceArr123(arrayIndexedInput) {
     let pushedArray = [];
-
     for(let i = 0; i < arrayIndexedInput.length; i++){
       if (arrayIndexedInput[i] < 10) {
         if (arrayIndexedInput[i] === 1) {
-          console.log(i);
-          console.log(arrayIndexedInput[i]);
           pushedArray.push(wordSubs[0]);
         } else if (arrayIndexedInput[i] === 2) {
           pushedArray.push(wordSubs[1]);
@@ -29,14 +26,12 @@ function robotTalk(input){
           pushedArray.push(wordSubs[2]);
         } else {
           pushedArray.push(i);
-        };
+        }
       } else {
         //create subarray via str conversion
         let strNum = arrayIndexedInput[i].toString();
         let digitArray = strNum.split("");
         pushedArray.push(digitArray);
-        console.log(pushedArray);
-
         //loop through subarrays, sort and reverse
         for (let j = 0; j < pushedArray[i].length; j++) {
           let numOverTen = pushedArray[i];
@@ -44,8 +39,6 @@ function robotTalk(input){
             return a-b
           });
           let reversed = sorted.reverse();
-          console.log(reversed);
-
           //conditions
           if (reversed.includes('3')){
             reversed.splice(pushedArray[i], i, wordSubs[2]);
@@ -55,9 +48,7 @@ function robotTalk(input){
             reversed.splice(pushedArray[i], i, wordSubs[0]);
           } else {
             reversed.push(i);
-            console.log(pushedArray[i][j]);
             numOverTen.splice(0,2);
-            console.log(pushedArray[i]);
             break;
           }
         }
@@ -76,8 +67,6 @@ $(document).ready(function () {
     //selectors
     const numberInput = parseInt($("#numberInput").val());
     const display = $(".display");
-    const submitBtn = $("#submit");
-
     //submit action
     let strConv = robotTalk(numberInput);
     display.append("<p>" + strConv + "</p>");
